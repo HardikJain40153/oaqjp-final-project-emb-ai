99f478522dca5ccd16f8af76808ac993de488038
Hardik Jain <hardikjain40153@gmail.com>
2026-08-16 09:50:58 -0400

    Task4

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,17 @@
+import json
+import requests
+
+def emotion_detector(text_to_analyze):
+    url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    payload = { "raw_document": { "text": text_to_analyze } }
+    response = requests.post(url = url,
+                             json = payload,
+                             headers = headers,
+                             timeout = 10)
+    formatted_response = json.loads(response.text)
+    emotion = formatted_response['emotionPredictions'][0]['emotion']
+    dominant_emotion = max(emotion, key=emotion.get)
+    emotion['dominant_emotion'] = dominant_emotion
+
+    return emotion
